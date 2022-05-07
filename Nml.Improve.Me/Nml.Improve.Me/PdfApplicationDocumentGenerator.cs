0caf37784662b32fd5974d25e56a851c3dd70699
201412176@student.uj.ac.za <951124Dimo>
2022-05-07 22:48:24 +0200

    NML project - Post changes

--- Nml.Improve.Me/Nml.Improve.Me/PdfApplicationDocumentGenerator.cs
+++ Nml.Improve.Me/Nml.Improve.Me/PdfApplicationDocumentGenerator.cs
@@ -1,141 +1,144 @@
-﻿using System;
+﻿using Nml.Improve.Me.Dependencies;
+using System;
 using System.Linq;
-using Nml.Improve.Me.Dependencies;
 
 namespace Nml.Improve.Me
 {
-	public class PdfApplicationDocumentGenerator : IApplicationDocumentGenerator
-	{
-		private readonly IDataContext DataContext;
-		private IPathProvider _templatePathProvider;
-		public IViewGenerator View_Generator;
-		internal readonly IConfiguration _configuration;
-		private readonly ILogger<PdfApplicationDocumentGenerator> _logger;
-		private readonly IPdfGenerator _pdfGenerator;
+    public class PdfApplicationDocumentGenerator
+        : IApplicationDocumentGenerator
+    {
+        private readonly IDataContext _dataContext;
+        private readonly IPathProvider _templatePathProvider;
+        private readonly IViewGenerator _viewGenerator;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<PdfApplicationDocumentGenerator> _logger;
+        private readonly IPdfGenerator _pdfGenerator;
 
-		public PdfApplicationDocumentGenerator(
-			IDataContext dataContext,
-			IPathProvider templatePathProvider,
-			IViewGenerator viewGenerator,
-			IConfiguration configuration,
-			IPdfGenerator pdfGenerator,
-			ILogger<PdfApplicationDocumentGenerator> logger)
-		{
-			if (dataContext != null)
-				throw new ArgumentNullException(nameof(dataContext));
-			
-			DataContext = dataContext;
-			_templatePathProvider = templatePathProvider ?? throw new ArgumentNullException("templatePathProvider");
-			View_Generator = viewGenerator;
-			_configuration = configuration;
-			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
-			_pdfGenerator = pdfGenerator;
-		}
-		
-		public byte[] Generate(Guid applicationId, string baseUri)
-		{
-			Application application = DataContext.Applications.Single(app => app.Id == applicationId);
+        public PdfApplicationDocumentGenerator(
+            IDataContext dataContext,
+            IPathProvider templatePathProvider,
+            IViewGenerator viewGenerator,
+            IConfiguration configuration,
+            IPdfGenerator pdfGenerator,
+            ILogger<PdfApplicationDocumentGenerator> logger)
+        {
+            if (dataContext != null)
+                throw new ArgumentNullException(nameof(dataContext));
 
-			if (application != null)
-			{
+            _dataContext = dataContext;
+            _templatePathProvider = templatePathProvider ?? throw new ArgumentNullException(nameof(templatePathProvider));
+            _viewGenerator = viewGenerator;
+            _configuration = configuration;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _pdfGenerator = pdfGenerator;
+        }
 
-				if (baseUri.EndsWith("/"))
-					baseUri = baseUri.Substring(baseUri.Length - 1);
+        public byte[] Generate(Guid applicationId, string baseUri)
+        {
+            string view = string.Empty;
+            string path = string.Empty;
 
-				string view;
+            Application application = _dataContext.Applications.Single(app => app.Id == applicationId);
 
-				if (application.State == ApplicationState.Pending)
-				{
-					string path = _templatePathProvider.Get("PendingApplication");
-					PendingApplicationViewModel vm = new PendingApplicationViewModel
-					{
-						ReferenceNumber = application.ReferenceNumber,
-						State = application.State.ToDescription(),
-						FullName = application.Person.FirstName + " " + application.Person.Surname,
-						AppliedOn = application.Date,
-						SupportEmail = _configuration.SupportEmail,
-						Signature = _configuration.Signature
-					};
-					view = View_Generator.GenerateFromPath(string.Format("{0}{1}", baseUri, path), vm);
-				}
-				else if (application.State == ApplicationState.Activated)
-				{
-					string path = _templatePathProvider.Get("ActivatedApplication");
-					ActivatedApplicationViewModel vm = new ActivatedApplicationViewModel
-					{
-						ReferenceNumber = application.ReferenceNumber,
-						State = application.State.ToDescription(),
-						FullName = $"{application.Person.FirstName} {application.Person.Surname}",
-						LegalEntity = application.IsLegalEntity ? application.LegalEntity : null,
-						PortfolioFunds = application.Products.SelectMany(p => p.Funds),
-						PortfolioTotalAmount = application.Products.SelectMany(p => p.Funds)
-														.Select(f => (f.Amount - f.Fees) * _configuration.TaxRate)
-														.Sum(),
-						AppliedOn = application.Date,
-						SupportEmail = _configuration.SupportEmail,
-						Signature = _configuration.Signature
-					};
-					view = View_Generator.GenerateFromPath(baseUri + path, vm);
-				}
-				else if (application.State == ApplicationState.InReview)
-				{
-					var templatePath = _templatePathProvider.Get("InReviewApplication");
-					var inReviewMessage = "Your application has been placed in review" +
-										application.CurrentReview.Reason switch
-										{
-											{ } reason when reason.Contains("address") =>
-												" pending outstanding address verification for FICA purposes.",
-											{ } reason when reason.Contains("bank") =>
-												" pending outstanding bank account verification.",
-											_ =>
-												" because of suspicious account behaviour. Please contact support ASAP."
-										};
-					var inReviewApplicationViewModel = new InReviewApplicationViewModel();
-					inReviewApplicationViewModel.ReferenceNumber = application.ReferenceNumber;
-					inReviewApplicationViewModel.State = application.State.ToDescription();
-					inReviewApplicationViewModel.FullName = string.Format(
-						"{0} {1}",
-						application.Person.FirstName,
-						application.Person.Surname);
-					inReviewApplicationViewModel.LegalEntity =
-						application.IsLegalEntity ? application.LegalEntity : null;
-					inReviewApplicationViewModel.PortfolioFunds = application.Products.SelectMany(p => p.Funds);
-					inReviewApplicationViewModel.PortfolioTotalAmount = application.Products.SelectMany(p => p.Funds)
-						.Select(f => (f.Amount - f.Fees) * _configuration.TaxRate)
-						.Sum();
-					inReviewApplicationViewModel.InReviewMessage = inReviewMessage;
-					inReviewApplicationViewModel.InReviewInformation = application.CurrentReview;
-					inReviewApplicationViewModel.AppliedOn = application.Date;
-					inReviewApplicationViewModel.SupportEmail = _configuration.SupportEmail;
-					inReviewApplicationViewModel.Signature = _configuration.Signature;
-					view = View_Generator.GenerateFromPath($"{baseUri}{templatePath}", inReviewApplicationViewModel);
-				}
-				else
-				{
-					_logger.LogWarning(
-						$"The application is in state '{application.State}' and no valid document can be generated for it.");
-					return null;
-				}
+            if (application != null)
+            {
+                if (baseUri.EndsWith("/"))
+                    baseUri = baseUri.Substring(baseUri.Length - 1);
 
-				var pdfOptions = new PdfOptions
-				{
-					PageNumbers = PageNumbers.Numeric,
-					HeaderOptions = new HeaderOptions
-					{
-						HeaderRepeat = HeaderRepeat.FirstPageOnly,
-						HeaderHtml = PdfConstants.Header
-					}
-				};
-				var pdf = _pdfGenerator.GenerateFromHtml(view, pdfOptions);
-				return pdf.ToBytes();
-			}
-			else
-			{
-				
-				_logger.LogWarning(
-					$"No application found for id '{applicationId}'");
-				return null;
-			}
-		}
-	}
+                if (application.State == ApplicationState.Pending)
+                {
+                    path = _templatePathProvider.Get("PendingApplication");
+
+                    PendingApplicationViewModel vm = new PendingApplicationViewModel
+                    {
+                        ReferenceNumber = application.ReferenceNumber,
+                        State = application.State.ToDescription(),
+                        FullName = $"{application.Person.FirstName} {application.Person.Surname}",
+                        AppliedOn = application.Date,
+                        SupportEmail = _configuration.SupportEmail,
+                        Signature = _configuration.Signature
+                    };
+
+                    view = _viewGenerator.GenerateFromPath($"{baseUri}{path}", vm);
+                }
+                else if (application.State == ApplicationState.Activated)
+                {
+                    path = _templatePathProvider.Get("ActivatedApplication");
+
+                    ActivatedApplicationViewModel vm = new ActivatedApplicationViewModel
+                    {
+                        ReferenceNumber = application.ReferenceNumber,
+                        State = application.State.ToDescription(),
+                        FullName = $"{application.Person.FirstName} {application.Person.Surname}",
+                        LegalEntity = application.IsLegalEntity ? application.LegalEntity : null,
+                        PortfolioFunds = application.Products.SelectMany(p => p.Funds),
+                        PortfolioTotalAmount = application.Products.SelectMany(p => p.Funds)
+                                                        .Select(f => (f.Amount - f.Fees) * _configuration.TaxRate)
+                                                        .Sum(),
+                        AppliedOn = application.Date,
+                        SupportEmail = _configuration.SupportEmail,
+                        Signature = _configuration.Signature
+                    };
+
+                    view = _viewGenerator.GenerateFromPath($"{baseUri}{path}", vm);
+                }
+                else if (application.State == ApplicationState.InReview)
+                {
+                    path = _templatePathProvider.Get("InReviewApplication");
+
+                    var inReviewMessage = "Your application has been placed in review" +
+                                        application.CurrentReview.Reason switch
+                                        {
+                                            { } reason when reason.Contains("address") =>
+                                                " pending outstanding address verification for FICA purposes.",
+                                            { } reason when reason.Contains("bank") =>
+                                                " pending outstanding bank account verification.",
+                                            _ =>
+                                                " because of suspicious account behaviour. Please contact support ASAP."
+                                        };
+
+                    InReviewApplicationViewModel vm = new InReviewApplicationViewModel
+                    {
+                        ReferenceNumber = application.ReferenceNumber,
+                        State = application.State.ToDescription(),
+                        FullName = $"{application.Person.FirstName} {application.Person.Surname}",
+                        LegalEntity = application.IsLegalEntity ? application.LegalEntity : null,
+                        PortfolioFunds = application.Products.SelectMany(p => p.Funds),
+                        PortfolioTotalAmount = application.Products.SelectMany(p => p.Funds)
+                                           .Select(f => (f.Amount - f.Fees) * _configuration.TaxRate).Sum(),
+                        InReviewMessage = inReviewMessage,
+                        InReviewInformation = application.CurrentReview,
+                        AppliedOn = application.Date,
+                        SupportEmail = _configuration.SupportEmail,
+                        Signature = _configuration.Signature,
+                    };
+
+                    view = _viewGenerator.GenerateFromPath($"{baseUri}{path}", vm);
+                }
+                else
+                {
+                    _logger.LogWarning($"The application is in state '{application.State}' and no valid document can be generated for it.");
+                    return null;
+                }
+
+                var pdfOptions = new PdfOptions
+                {
+                    PageNumbers = PageNumbers.Numeric,
+                    HeaderOptions = new HeaderOptions
+                    {
+                        HeaderRepeat = HeaderRepeat.FirstPageOnly,
+                        HeaderHtml = PdfConstants.Header
+                    }
+                };
+
+                var pdf = _pdfGenerator.GenerateFromHtml(view, pdfOptions);
+                return pdf.ToBytes();
+            }
+            else
+            {
+                _logger.LogWarning($"No application found for id '{applicationId}'");
+                return null;
+            }
+        }
+    }
 }
